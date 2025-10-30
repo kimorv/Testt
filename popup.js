f6d4ad0195285923724bf73908bf49f080b00828
WalidBensallam <166942531+kimorv@users.noreply.github.com>
2025-10-30 16:07:12 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.getElementById('sayHi').addEventListener('click', async () => {
+let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+                func: () => alert('Hello from your extension!')
+                  });
+                  });
